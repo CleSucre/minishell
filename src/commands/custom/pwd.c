b7bf99ba38cc5643733d21f847f67284283ec9af
CleSucre <julien.thomas.4321@gmail.com>
2024-09-10 19:08:20 +0200

    Revert "Maxou"

--- src/commands/custom/pwd.c
+++ src/commands/custom/pwd.c
@@ -12,24 +12,7 @@
 
 #include "minishell.h"
 
-/** https://github.com/bminor/bash/blob/master/builtins/cd.def
- * @brief Print the current working directory
- *
- * @param cmd The command structure
- *
- * @info
- * from bash source code :
- * static int bindpwd PARAMS((int));
- *	static int setpwd PARAMS((char *));
- *	static char *resetpwd PARAMS((char *));
- *	static int change_to_directory PARAMS((char *, int, int));
- */
-
 void	command_pwd(t_cmd *cmd)
 {
-	char *path;
-
-	path = getenv("PWD");
-	ft_printf("Le path : [%s]\n", path);
 	(void)cmd;
 }
