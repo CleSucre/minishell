dea0aaf966334a7b9ff0d9e6b079f56b882c4e00
mpierrot <pierrot.maxime.pro@gmail.com>
2024-10-17 08:10:28 +0200

    cd look fixed if oldpwd unset

--- src/commands/custom/cd_utils.c
+++ src/commands/custom/cd_utils.c
@@ -14,13 +14,11 @@
 
 int	check_oldpwd(t_minishell *minishell, t_cmd *cmd)
 {
-	char	**tmp;
+	char	buffer[BUFFER_SIZE];
 
-	if (find_table_args(minishell->env, "OLDPWD") != -1)
+	if (find_table_args(cmd->env, "OLDPWD") != -1)
 		return (0);
-	tmp = ft_tabdup((const char **)cmd->env);
-	add_cmd_env(minishell, "OLDPWD", getcwd(NULL, 0));
-	cmd->env = ft_tabinsert(tmp, ft_strjoin("OLDPWD=", getcwd(NULL, 0)),
-			ft_tablen((const char **)cmd->env) - 1);
+	add_cmd_env(minishell, "OLDPWD", getcwd(buffer, BUFFER_SIZE));
+	cmd->env = minishell->env;
 	return (1);
 }
