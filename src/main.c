784e28c7b93873d5f1270701d3d9abd1891d60e7
CleSucre <47931504+CleSucre@users.noreply.github.com>
2024-10-17 22:18:32 +0200

    cannot run ./minishell in a pipe line anymore

--- src/main.c
+++ src/main.c
@@ -13,6 +13,19 @@
 #include "minishell.h"
 
 /**
+ * @brief Check if the shell is running in
+ * 			a pipe (stdin or stdout not a terminal)
+ *
+ * @return int 1 if stdin or stdout is not a terminal, 0 otherwise.
+ */
+int	is_running_in_pipeline(void)
+{
+	if (!isatty(STDIN_FILENO) || !isatty(STDOUT_FILENO))
+		return (1);
+	return (0);
+}
+
+/**
  * @brief Main function of the minishell
  *
  * @param int argc
@@ -28,6 +41,11 @@ int	main(int argc, char **args, char **env)
 
 	(void)argc;
 	(void)args;
+	if (is_running_in_pipeline())
+	{
+		fprintf(stderr, "Error: minishell cannot be run in a pipeline.\n");
+		return (1);
+	}
 	minishell = alloc_minishell();
 	minishell->term->original_termios = &original_termios;
 	minishell->env = ft_tabdup((const char **)env);
